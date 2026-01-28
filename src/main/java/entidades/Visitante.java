20b02ec1861e50a1a5930778638d2eff2b59f936
CamiloOrtiz1 <camilo.ortiz@est.fi.uncoma.edu.ar>
2026-01-28 19:09:07 -0300

    feat: Funcionalidad nueva sobre visitante

---
+++ src/main/java/entidades/Visitante.java
@@ -0,0 +1,14 @@
+package entidades;
+
+public class Visitante {
+
+    private String nombre;
+
+    public Visitante(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+}
